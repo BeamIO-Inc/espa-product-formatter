4b48f3723922411de91f5c261ae04ad1d574b4de
Gail Schmidt <gschmidt@usgs.gov>
2015-08-27 10:38:39 -0600

    Added support for generating Julian date bands

---
+++ src/raw_binary/format_conversion_libs/generate_julian_date_bands.h
@@ -0,0 +1,46 @@
+/*****************************************************************************
+FILE: generate_julian_date_bands
+  
+PURPOSE: Contains defines and prototypes to generate a Julian date/year band.
+
+PROJECT:  Land Satellites Data System Science Research and Development (LSRD)
+at the USGS EROS
+
+LICENSE TYPE:  NASA Open Source Agreement Version 1.3
+
+HISTORY:
+Date         Programmer       Reason
+----------   --------------   -------------------------------------
+8/26/2015    Gail Schmidt     Original development
+
+NOTES:
+*****************************************************************************/
+
+#ifndef GENERATE_JULIAN_BAND_H
+#define GENERATE_JULIAN_BAND_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "error_handler.h"
+#include "espa_metadata.h"
+#include "parse_metadata.h"
+#include "write_metadata.h"
+#include "raw_binary_io.h"
+#include "envi_header.h"
+
+/* Defines */
+
+/* Prototypes */
+int generate_julian_date_bands
+(
+    Espa_internal_meta_t *xml_meta,  /* I: input XML metadata */
+    unsigned int **jdate_band,       /* O: pointer to Julian date buffer with
+                                           year*1000 + DOY */
+    unsigned short **doy_band,       /* O: pointer to Julian DOY buffer */
+    unsigned short **year_band,      /* O: pointer to year buffer */
+    int *nlines,                     /* O: number of lines in Julian bands */
+    int *nsamps                      /* O: number of samples in Julian bands */
+);
+
+#endif
