e672eb6f2a26f80bcef0d95267c9a0db389f1086
Gail Schmidt <gail.schmidt.ctr@usgs.gov>
2019-09-22 15:13:55 -0500

    Updated the version number in the header

--- raw_binary/common/espa_common.h
+++ raw_binary/common/espa_common.h
@@ -14,7 +14,7 @@ NOTES:
 #ifndef ESPA_COMMON_H_
 #define ESPA_COMMON_H_
 
-#define ESPA_COMMON_VERSION "1.18.0"
+#define ESPA_COMMON_VERSION "1.18.1"
 
 /* Set up default success/error defines */
 #define SUCCESS 0
